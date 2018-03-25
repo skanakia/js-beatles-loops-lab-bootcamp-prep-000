f30d6848589291c0412ed65a659f3cc74f655731
Sameer Kanakia <skanakia1@gmail.com>
2018-03-25 14:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (let i=0, i<musicians.length, i++)
+  for (let i=0, i<musicians.length, i++) {
   array.push("${musicians[i]} plays ${instuments[i]")
+  }
+  return array
 }
