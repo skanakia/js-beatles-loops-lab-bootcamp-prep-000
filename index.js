899edd7767f5c881ac2a4b4eeb90ba3523923876
Sameer Kanakia <skanakia1@gmail.com>
2018-03-25 14:39:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (let i=0; i<musicians.length; i++) {
-  array.push(${musicians[i]} + " plays " + ${instuments[i])
+  array.push('${musicians[i]} plays ${instuments[i]}')
   }
   return array
 }
