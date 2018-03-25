c0e9865baf33801455ca2004e6c625d27c8428de
Sameer Kanakia <skanakia1@gmail.com>
2018-03-25 14:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(arr1, arr2) {
   var newarray = [];
   for (var i=0; i < arr1.length; i++) {
-  newarray.push('${arr1[i]} plays ${arr2[i]}');
+    newarray.push('${arr1[i]} plays ${arr2[i]}');
   }
   return newarray
 }
